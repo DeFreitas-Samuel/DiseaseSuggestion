db167c304c9ea71f3ae86543c6b45fde0d664cdc
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-09-30 10:37:18 -0400

    Registradas nuestras pages y sus respectivos view models en el RegisterTypes

--- ProyectoFinalXamarin/ProyectoFinalXamarin/App.xaml.cs
+++ ProyectoFinalXamarin/ProyectoFinalXamarin/App.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using ProyectoFinalXamarin.ViewModels;
+using ProyectoFinalXamarin.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ProyectoFinalXamarin
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+            
+        }
+
+        protected override void OnInitialized()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
         }
 
-        protected override void OnStart()
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-        }
-
-        protected override void OnSleep()
-        {
-        }
-
-        protected override void OnResume()
-        {
+            containerRegistry.RegisterForNavigation<HomePage, HomeViewModel>();
+            containerRegistry.RegisterForNavigation<AnalysisCompletedPage, AnalysisCompletedViewModel>();
+            containerRegistry.RegisterForNavigation<RecommendTestPage, RecommendTestViewModel>();
+            containerRegistry.RegisterForNavigation<ResultsPage, ResultsViewModel>();
         }
     }
 }
---
+++ ProyectoFinalXamarin/ProyectoFinalXamarin/ViewModels/RecommendTestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalXamarin.ViewModels
+{
+    class RecommendTestViewModel
+    {
+    }
+}
