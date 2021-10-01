ba7d48a899c8c9560bb66d9387ad3045acb7dc28
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-09-30 22:40:50 -0400

    La navegacion fue implementada en todas las pages

--- ProyectoFinalXamarin/ProyectoFinalXamarin/ViewModels/AnalysisCompletedViewModel.cs
+++ ProyectoFinalXamarin/ProyectoFinalXamarin/ViewModels/AnalysisCompletedViewModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ProyectoFinalXamarin.ViewModels
 {
+    
     class AnalysisCompletedViewModel
     {
+        public ICommand NavigateResultCommand { get; }
+        public ICommand NavigateRecommendationCommand { get; }
+        public AnalysisCompletedViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NavigateResultCommand = new DelegateCommand(OnNavigationResults);
+            NavigateRecommendationCommand = new DelegateCommand(OnNavigationRecommendations);
+        }
+
+        private async void OnNavigationRecommendations() => await _navigationService.NavigateAsync("RecommendTestPage");
+
+
+        private async void OnNavigationResults() => await _navigationService.NavigateAsync("ResultsPage");
+
+        INavigationService _navigationService;
     }
 }
--- ProyectoFinalXamarin/ProyectoFinalXamarin/ViewModels/HomeViewModel.cs
+++ ProyectoFinalXamarin/ProyectoFinalXamarin/ViewModels/HomeViewModel.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ProyectoFinalXamarin.ViewModels
 {
     class HomeViewModel
     {
+        public ICommand NavigateCommand { get; }
+        public HomeViewModel(INavigationService navigationService) 
+        {
+
+            _navigationService = navigationService;
+            NavigateCommand = new DelegateCommand(OnNavigation); 
+        }
+
+        private async void OnNavigation() => await _navigationService.NavigateAsync("AnalysisCompletedPage");
+
+
+        INavigationService _navigationService;
     }
 }
