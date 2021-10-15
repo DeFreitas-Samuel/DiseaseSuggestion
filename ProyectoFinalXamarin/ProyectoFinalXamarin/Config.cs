aa60111d75eb9b46e6aadf112877a044f5c75cce
Vladimir <vladimirfranciscodiaz@gmail.com>
2021-10-15 19:21:54 -0400

    Delete unnecessary URLs from Config

--- ProyectoFinalXamarin/ProyectoFinalXamarin/Config.cs
+++ ProyectoFinalXamarin/ProyectoFinalXamarin/Config.cs
@@ -6,9 +6,6 @@ namespace ProyectoFinalXamarin
 {
     public static class Config
     {
-        public const string GetSuggestionsURL = "https://api.endlessmedical.com/v1/dx/GetSuggestedSpecializations?SessionID={SessionId}&NumberOfResults=10";
-        public const string GetOutcomesURL = "https://api.endlessmedical.com/v1/dx/GetOutcomes";
-        public const string GetDiseasesURL = "https://api.endlessmedical.com/";
         public const string LoginUrl = "https://sandbox-authservice.priaid.ch/login";
     }
 }
